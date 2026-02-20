ffe1c1de52486cd1a297dbed1610cde122d580c3
SAJEB KHAN <sajebk007@gmail.com>
2026-02-20 17:12:02 +0530

    api gateway

--- Backend/listing-service/src/main/java/com/eventsphere/listing_service/controller/ListingController.java
+++ Backend/listing-service/src/main/java/com/eventsphere/listing_service/controller/ListingController.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/listing")
+@RequestMapping("/listings")
 public class ListingController {
 
     @Autowired
