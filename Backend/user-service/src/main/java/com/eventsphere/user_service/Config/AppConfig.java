044a85f253cfbd190b697337b912c05bef5fc8a3
SAJEB KHAN <sajebk007@gmail.com>
2026-02-21 23:34:45 +0530

    eureka server

--- Backend/user-service/src/main/java/com/eventsphere/user_service/Config/AppConfig.java
+++ Backend/user-service/src/main/java/com/eventsphere/user_service/Config/AppConfig.java
@@ -27,7 +27,7 @@ public class AppConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration){
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
